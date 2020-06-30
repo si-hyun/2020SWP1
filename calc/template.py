6696839fa61546062d1d6d45372e68e8af272126
si-hyun <tlgus31@gmail.com>
2020-06-29 23:44:48 -0700

    issue #4

---
+++ calc/template.py
@@ -0,0 +1,13 @@
+html = """
+<html>
+    <body>
+        <form action="">
+	    a = <input type="number" name="a"><br>
+	    b = <input type="number" name="b"><br>
+	    <input type="submit">
+	</form>
+	sum = %(sum)d <br>
+	multi = &(multi)d
+    </body>
+</html>
+"""
